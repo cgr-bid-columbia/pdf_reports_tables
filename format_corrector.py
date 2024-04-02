9e7071ecffe6a695009e7972f31c7b5ae2c8f525
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2024-04-02 15:40:33 -0400

    fix value column indexing and empty row dropping

--- format_corrector.py
+++ format_corrector.py
@@ -12,8 +12,8 @@ df = pd.read_excel(excel_file)
 current_phrase = "" 
 
 # iterate through row column 
-for index, row in df.iterrows(): 
-    cell_value = row['row'] 
+for index, cell_value in df['row'].items(): 
+    cell_value = str(cell_value)
 
     if cell_value.endswith(":"): 
         # add current cell ending with : to current_phrase 
@@ -27,21 +27,17 @@ for index, row in df.iterrows():
         # add cell to current phrase until colon is found 
         current_phrase += cell_value 
 
-# drop empty cells in column 
-df['row'].replace('', pd.NA, inplace=True) 
-
 # fix 'value' column 
-df.at[2, 'value'] += " " + df.at[3, 'value'] + " " + df.at[4, 'value'] 
-df.at[7, 'value'] += " " + df.at[8, 'value'] + " " + df.at[9, 'value'] + " " + df.at[10, 'value'] + " " + df.at[11, 'value'] 
-# special case for 15, 16 
-indicator_words = df.at[15, 'value'].split() 
-value_words = df.at[16, 'value'].split() 
-value_words = value_words[0:2] + [value_words[2] + " " + value_words[3]]
-df.at[15, 'value'] = str([indicator_words[i] + " " + value_words[i] for i in range(len(indicator_words))])
-df.at[19, 'value'] += " " + df.at[20, 'value']
+df['value'] = df['value'].astype(str) 
+df.iloc[2, 2] += " " + df.iloc[3, 2] + " " + df.iloc[4, 2] 
+df.iloc[7, 2] += " " + df.iloc[8, 2] + " " + df.iloc[9, 2] + " " + df.iloc[10, 2] + " " + df.iloc[11, 2] 
+df.iloc[15, 2] += " " + df.iloc[16, 2] 
+df.iloc[19, 2] += " " + df.iloc[20, 2]
 
 # drop irrelevant rows 
-df.drop([1, 3, 4, 6, 8, 9, 10, 11, 14, 16, 18, 20], inplace=True) 
+df.dropna(subset=['row'], inplace=True)
+df.dropna(subset=['value'], inplace=True)
+
 
 # renumber initial column 
 df.iloc[:, 0] = range(0, len(df)) 
