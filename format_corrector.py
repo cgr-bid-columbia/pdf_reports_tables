e30fc92b28c07a76461fb3df3be8c14270e1663c
jrp8427 <70613110+jrp8427@users.noreply.github.com>
2024-04-02 16:00:04 -0400

    correct spaces within phrases

--- format_corrector.py
+++ format_corrector.py
@@ -6,14 +6,13 @@ excel_file = "000002-2021-CG-APP-ficha_resumen_url_table_1.xlsx"
 # read Excel file into pandas dataframe 
 df = pd.read_excel(excel_file)
 
-# fix 'row' column 
-
 # create variable to store current phrase being evaluated 
 current_phrase = "" 
 
 # iterate through row column 
 for index, cell_value in df['row'].items(): 
-    cell_value = str(cell_value)
+    # fill empty rows with empty strings 
+    df.fillna({'row': ''}, inplace=True)
 
     if cell_value.endswith(":"): 
         # add current cell ending with : to current_phrase 
@@ -21,23 +20,26 @@ for index, cell_value in df['row'].items():
 
         # update cell with full combined phrase 
         df.at[index, 'row'] = current_phrase 
-
         current_phrase = "" 
-    else: 
-        # add cell to current phrase until colon is found 
-        current_phrase += cell_value 
 
-# fix 'value' column 
+    elif cell_value != '': 
+        # add cell to current phrase until colon is found 
+        current_phrase += cell_value + " "
+        # replace incomplete rows with empty strings 
+        df.at[index, 'row'] = "" 
+
+# fill empty value rows with empty strings 
+df.fillna({'value': ''}, inplace=True)
+# turn all value columns to strings 
 df['value'] = df['value'].astype(str) 
+# manually corrected rows, will somehow automate to account for other formats 
 df.iloc[2, 2] += " " + df.iloc[3, 2] + " " + df.iloc[4, 2] 
 df.iloc[7, 2] += " " + df.iloc[8, 2] + " " + df.iloc[9, 2] + " " + df.iloc[10, 2] + " " + df.iloc[11, 2] 
 df.iloc[15, 2] += " " + df.iloc[16, 2] 
 df.iloc[19, 2] += " " + df.iloc[20, 2]
 
 # drop irrelevant rows 
-df.dropna(subset=['row'], inplace=True)
 df.dropna(subset=['value'], inplace=True)
-
 
 # renumber initial column 
 df.iloc[:, 0] = range(0, len(df)) 
