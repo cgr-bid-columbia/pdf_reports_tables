20b68aa61f45418b7802fca1117ae374babf1224
Marco Gutierrez <a20141676@pucp.pe>
2024-07-18 17:42:58 -0400

    quickfix table_2_parse.py: try/exc for unrecognized formats

--- table_2_scripts/table_2_parser.py
+++ table_2_scripts/table_2_parser.py
@@ -7,8 +7,8 @@ import unicodedata, logging, json, os
 JSON_FILE = "paths.json"
 pdf_tables_file = open(JSON_FILE, "r")
 pdf_tables_dict = json.load(pdf_tables_file)
-# TASK_ID = os.environ.get("SLURM_ARRAY_TASK_ID")
-TASK_ID = 1
+TASK_ID = os.environ.get("SLURM_ARRAY_TASK_ID")
+# TASK_ID = 1
 LOGS_FOLDER = "./logs"
 LOGNAME = f"/data_corrector_{TASK_ID}.log"
 NUM_JOBS = pdf_tables_dict["num_jobs"]
@@ -31,112 +31,119 @@ def table_2_data_corrector(excel_file: str, output_path: str) -> pd.DataFrame:
     data = pd.read_excel(excel_file).iloc[:, 1:] # reading the file and removing the first column
     print("data: ", data)
 
-    report = {"filename": excel_file, "num_rows": len(data), "num_control_types": 0} # placeholder for output report
+    report = {"filename": excel_file, "num_rows": len(data), 
+              "num_control_types": 0, "num_rows_post_cleaning": "-"} # placeholder for output report
 
     # placeholder for cleaned data
     cleaned_data = {"control_type": [], "indicator": []}
 
-    # checking format of first rows
+    try: 
+        # checking format of first rows
+        if len(data) >= 2:
+            first_item_row = data["row"].values[0]
+            first_item_values = data["value"].values[0]
+            second_item_row = data["row"].values[1]
 
-    if len(data) >= 2:
-        first_item_row = data["row"].values[0]
-        first_item_values = data["value"].values[0]
-        second_item_row = data["row"].values[1]
+            if ("item" in str(first_item_row).lower() or "unnamed" in str(first_item_row).lower()) and ("contrato" not in str(second_item_row).lower()): # format 1
+                
+                # remove first row    
+                data = data.iloc[1:] 
 
-        if ("item" in str(first_item_row).lower() or "unnamed" in str(first_item_row).lower()) and ("contrato" not in str(second_item_row).lower()): # format 1
+                # removing first column
+                data = data.iloc[:, 1:]
+
+                # finding the control type(s)
+                for _, row in data.iterrows():
+                    if " X" in str(row.values): # tagging the control type
+                        cleaned_data["control_type"].append(row.values[0].replace(" X", "").strip().lower())
+                        cleaned_data["indicator"].append("yes")
+                        report["num_control_types"] += 1
+                    else:
+                        cleaned_data["control_type"].append(row.values[0].strip().lower())
+                        cleaned_data["indicator"].append("no")
+                
+            elif "contrato" in str(first_item_row).lower() or "contrato" in str(second_item_row).lower(): # format 2
+
+                if "contrato" in str(first_item_row).lower(): # format 2.1
+                    if "bien" in str(first_item_values).lower(): # format 2.1.1
+
+                        # removing first column
+                        data = data.iloc[:, 1:]
+
+                        # finding the control type(s)
+                        for _, row in data.iterrows(): 
+                            if "X" in str(row.values): # tagging the control type
+                                cleaned_data["control_type"].append(row.values[0].replace("X", "").strip().lower())
+                                cleaned_data["indicator"].append("yes")
+                                report["num_control_types"] += 1
+
+                            else:
+                                cleaned_data["control_type"].append(row.values[0].strip().lower())
+                                cleaned_data["indicator"].append("no")
+                    
+                    elif "bien" in str(second_item_row).lower(): # format 2.1.2
+
+                        for _, row in data.iterrows():
+                            if "X" in str(row.values): # tagging the control type
+                                cleaned_data["control_type"].append(row.row.strip().lower())
+                                cleaned_data["indicator"].append("yes")
+                                report["num_control_types"] += 1
+
+                            else:
+                                cleaned_data["control_type"].append(row.row.strip().lower())
+                                cleaned_data["indicator"].append("no")
+                    
+                    else:
+                        pass
+                
+                elif "contrato" in str(second_item_row).lower(): # format 2.2
+                    
+                    # remove first and second rows
+                    data = data.iloc[2:]
+                    
+                    if "bien" in str(first_item_values).lower(): # format 2.2.1
+
+                        # removing first column
+                        data = data.iloc[:, 1:]
+
+                        # finding the control type(s)
+                        for _, row in data.iterrows(): 
+                            if "X" in str(row.values): # tagging the control type
+                                cleaned_data["control_type"].append(row.values[0].replace("X", "").strip().lower())
+                                cleaned_data["indicator"].append("yes")
+                                report["num_control_types"] += 1
+
+                            else:
+                                cleaned_data["control_type"].append(row.values[0].strip().lower())
+                                cleaned_data["indicator"].append("no")
+                    
+                    elif "bien" in str(second_item_row).lower(): # format 2.2.2
+
+                        for _, row in data.iterrows():
+                            if "X" in str(row.values): # tagging the control type
+                                cleaned_data["control_type"].append(row.row.strip().lower())
+                                cleaned_data["indicator"].append("yes")
+                                report["num_control_types"] += 1
+
+                            else:
+                                cleaned_data["control_type"].append(row.row.strip().lower())
+                                cleaned_data["indicator"].append("no")
+                    
+                    else:
+                        pass
             
-            # remove first row    
-            data = data.iloc[1:] 
+            else:
+                pass
 
-            # removing first column
-            data = data.iloc[:, 1:]
-
-            # finding the control type(s)
-            for _, row in data.iterrows():
-                if " X" in str(row.values): # tagging the control type
-                    cleaned_data["control_type"].append(row.values[0].replace(" X", "").strip().lower())
-                    cleaned_data["indicator"].append("yes")
-                    report["num_control_types"] += 1
-                else:
-                    cleaned_data["control_type"].append(row.values[0].strip().lower())
-                    cleaned_data["indicator"].append("no")
-            
-        elif "contrato" in str(first_item_row).lower() or "contrato" in str(second_item_row).lower(): # format 2
-
-            if "contrato" in str(first_item_row).lower(): # format 2.1
-                if "bien" in str(first_item_values).lower(): # format 2.1.1
-
-                    # removing first column
-                    data = data.iloc[:, 1:]
-
-                    # finding the control type(s)
-                    for _, row in data.iterrows(): 
-                        if "X" in str(row.values): # tagging the control type
-                            cleaned_data["control_type"].append(row.values[0].replace("X", "").strip().lower())
-                            cleaned_data["indicator"].append("yes")
-                            report["num_control_types"] += 1
-
-                        else:
-                            cleaned_data["control_type"].append(row.values[0].strip().lower())
-                            cleaned_data["indicator"].append("no")
-                
-                elif "bien" in str(second_item_row).lower(): # format 2.1.2
-
-                    for _, row in data.iterrows():
-                        if "X" in str(row.values): # tagging the control type
-                            cleaned_data["control_type"].append(row.row.strip().lower())
-                            cleaned_data["indicator"].append("yes")
-                            report["num_control_types"] += 1
-
-                        else:
-                            cleaned_data["control_type"].append(row.row.strip().lower())
-                            cleaned_data["indicator"].append("no")
-            
-            elif "contrato" in str(second_item_row).lower(): # format 2.2
-                
-                # remove first and second rows
-                data = data.iloc[2:]
-                
-                if "bien" in str(first_item_values).lower(): # format 2.2.1
-
-                    # removing first column
-                    data = data.iloc[:, 1:]
-
-                    # finding the control type(s)
-                    for _, row in data.iterrows(): 
-                        if "X" in str(row.values): # tagging the control type
-                            cleaned_data["control_type"].append(row.values[0].replace("X", "").strip().lower())
-                            cleaned_data["indicator"].append("yes")
-                            report["num_control_types"] += 1
-
-                        else:
-                            cleaned_data["control_type"].append(row.values[0].strip().lower())
-                            cleaned_data["indicator"].append("no")
-                
-                elif "bien" in str(second_item_row).lower(): # format 2.2.2
-
-                    for _, row in data.iterrows():
-                        if "X" in str(row.values): # tagging the control type
-                            cleaned_data["control_type"].append(row.row.strip().lower())
-                            cleaned_data["indicator"].append("yes")
-                            report["num_control_types"] += 1
-
-                        else:
-                            cleaned_data["control_type"].append(row.row.strip().lower())
-                            cleaned_data["indicator"].append("no")
-                
-
-        cleaned_df = pd.DataFrame(cleaned_data)
-        report["num_rows_post_cleaning"] = len(cleaned_df)
-        print("cleaned_df: ", cleaned_df)
-        
-        cleaned_df.to_csv(output_path + f"/{excel_file.split('/')[-1]}" + ".csv", index=False, sep="|")
-        return pd.DataFrame([report])
+            cleaned_df = pd.DataFrame(cleaned_data)
+            cleaned_df.to_csv(output_path + f"/{excel_file.split('/')[-1]}" + ".csv", index=False, sep="|")
+            report["num_rows_post_cleaning"] = len(cleaned_df)
+            print("cleaned_df: ", cleaned_df)
     
-    else:
+    except:
         report["num_control_types"] = "-"
-        report["num_rows_post_cleaning"] = "-"
-        return pd.DataFrame([report])
+    
+    return pd.DataFrame([report])
 
 
 def divide_chunks(list_input: list, num_chunks: int) -> list:
@@ -152,7 +159,7 @@ if __name__ == "__main__":
     # read paths
     input_path = pdf_tables_dict["input_path"] 
     output_path = pdf_tables_dict["output_path"]
-    reports_path = pdf_tables_dict["output_path"]
+    reports_path = pdf_tables_dict["reports_path"]
     
     files_paths = glob(input_path + "/*.xlsx", recursive=True) # reading pdfs from input path
     paths_splitted = list(divide_chunks(files_paths, int(len(files_paths)/NUM_JOBS)))
