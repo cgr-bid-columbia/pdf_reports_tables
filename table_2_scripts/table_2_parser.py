dd3d8e391dcbd19ba2513ba5d912018a1fe5740a
Marco Gutierrez <a20141676@pucp.pe>
2024-07-18 18:05:16 -0400

    quickfix table_2_parser: forward slash reports

--- table_2_scripts/table_2_parser.py
+++ table_2_scripts/table_2_parser.py
@@ -176,4 +176,4 @@ if __name__ == "__main__":
             formatting_reports.append(current_report)
     
     # exporting formatting reports
-    pd.concat(formatting_reports).to_csv(reports_path + r"\formatting_reports" + f"_{TASK_ID}.csv", sep="|", index=False)
+    pd.concat(formatting_reports).to_csv(reports_path + r"/formatting_reports" + f"_{TASK_ID}.csv", sep="|", index=False)
